7c234ea7e8d55b31610a28f545359813650346e4
liuhaoeric <liuhaoeric@didichuxing.com>
2018-11-04 22:07:01 +0800

    DefaultPromise.java AbstractChannel.java

--- example/src/main/java/io/netty/example/echo/EchoClientHandler.java
+++ example/src/main/java/io/netty/example/echo/EchoClientHandler.java
@@ -33,6 +33,7 @@ public class EchoClientHandler extends ChannelInboundHandlerAdapter {
      * Creates a client-side handler.
      */
     public EchoClientHandler() {
+        //todo byteBuf
         firstMessage = Unpooled.buffer(EchoClient.SIZE);
         for (int i = 0; i < firstMessage.capacity(); i ++) {
             firstMessage.writeByte((byte) i);
